de1900d920a4c217c91560ec8a08a86978d2a398
Jeyason Jeyaparan <56597078+jey12345678@users.noreply.github.com>
2021-07-11 23:53:37 -0400

    Add files via upload

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.dashboardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addNewDashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewBookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addNewDashboardToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.viewDashboardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewDashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.addClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewClientInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewBookToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.returnDashboardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.completeDashboardDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -55,7 +56,7 @@
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1129, 33);
+            this.menuStrip1.Size = new System.Drawing.Size(1185, 58);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -64,77 +65,102 @@
             this.dashboardsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addNewDashboardToolStripMenuItem1,
             this.viewDashboardsToolStripMenuItem});
+            this.dashboardsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("dashboardsToolStripMenuItem.Image")));
+            this.dashboardsToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.dashboardsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.dashboardsToolStripMenuItem.Name = "dashboardsToolStripMenuItem";
-            this.dashboardsToolStripMenuItem.Size = new System.Drawing.Size(124, 29);
+            this.dashboardsToolStripMenuItem.Size = new System.Drawing.Size(174, 54);
             this.dashboardsToolStripMenuItem.Text = "Dashboards";
+            // 
+            // addNewDashboardToolStripMenuItem1
+            // 
+            this.addNewDashboardToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("addNewDashboardToolStripMenuItem1.Image")));
+            this.addNewDashboardToolStripMenuItem1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.addNewDashboardToolStripMenuItem1.Name = "addNewDashboardToolStripMenuItem1";
+            this.addNewDashboardToolStripMenuItem1.Size = new System.Drawing.Size(307, 60);
+            this.addNewDashboardToolStripMenuItem1.Text = "Add New Dashboard";
+            // 
+            // viewDashboardsToolStripMenuItem
+            // 
+            this.viewDashboardsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewDashboardsToolStripMenuItem.Image")));
+            this.viewDashboardsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.viewDashboardsToolStripMenuItem.Name = "viewDashboardsToolStripMenuItem";
+            this.viewDashboardsToolStripMenuItem.Size = new System.Drawing.Size(307, 60);
+            this.viewDashboardsToolStripMenuItem.Text = "View Dashboards";
             // 
             // addNewDashboardToolStripMenuItem
             // 
             this.addNewDashboardToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addClientToolStripMenuItem,
             this.viewClientInfoToolStripMenuItem});
+            this.addNewDashboardToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addNewDashboardToolStripMenuItem.Image")));
+            this.addNewDashboardToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.addNewDashboardToolStripMenuItem.Name = "addNewDashboardToolStripMenuItem";
-            this.addNewDashboardToolStripMenuItem.Size = new System.Drawing.Size(72, 29);
+            this.addNewDashboardToolStripMenuItem.Size = new System.Drawing.Size(112, 54);
             this.addNewDashboardToolStripMenuItem.Text = "Client";
             this.addNewDashboardToolStripMenuItem.Click += new System.EventHandler(this.addNewDashboardToolStripMenuItem_Click);
             // 
-            // viewBookToolStripMenuItem
-            // 
-            this.viewBookToolStripMenuItem.Name = "viewBookToolStripMenuItem";
-            this.viewBookToolStripMenuItem.Size = new System.Drawing.Size(163, 29);
-            this.viewBookToolStripMenuItem.Text = "Give Dashboards";
-            // 
-            // addNewDashboardToolStripMenuItem1
-            // 
-            this.addNewDashboardToolStripMenuItem1.Name = "addNewDashboardToolStripMenuItem1";
-            this.addNewDashboardToolStripMenuItem1.Size = new System.Drawing.Size(281, 34);
-            this.addNewDashboardToolStripMenuItem1.Text = "Add New Dashboard";
-            // 
-            // viewDashboardsToolStripMenuItem
-            // 
-            this.viewDashboardsToolStripMenuItem.Name = "viewDashboardsToolStripMenuItem";
-            this.viewDashboardsToolStripMenuItem.Size = new System.Drawing.Size(281, 34);
-            this.viewDashboardsToolStripMenuItem.Text = "View Dashboards";
-            // 
             // addClientToolStripMenuItem
             // 
+            this.addClientToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addClientToolStripMenuItem.Image")));
+            this.addClientToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.addClientToolStripMenuItem.Name = "addClientToolStripMenuItem";
-            this.addClientToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.addClientToolStripMenuItem.Size = new System.Drawing.Size(263, 60);
             this.addClientToolStripMenuItem.Text = "Add Client";
             // 
             // viewClientInfoToolStripMenuItem
             // 
+            this.viewClientInfoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewClientInfoToolStripMenuItem.Image")));
+            this.viewClientInfoToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.viewClientInfoToolStripMenuItem.Name = "viewClientInfoToolStripMenuItem";
-            this.viewClientInfoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.viewClientInfoToolStripMenuItem.Size = new System.Drawing.Size(263, 60);
             this.viewClientInfoToolStripMenuItem.Text = "View Client Info";
+            // 
+            // viewBookToolStripMenuItem
+            // 
+            this.viewBookToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewBookToolStripMenuItem.Image")));
+            this.viewBookToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.viewBookToolStripMenuItem.Name = "viewBookToolStripMenuItem";
+            this.viewBookToolStripMenuItem.Size = new System.Drawing.Size(232, 54);
+            this.viewBookToolStripMenuItem.Text = "Assign Dashboards";
             // 
             // returnDashboardsToolStripMenuItem
             // 
+            this.returnDashboardsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("returnDashboardsToolStripMenuItem.Image")));
+            this.returnDashboardsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.returnDashboardsToolStripMenuItem.Name = "returnDashboardsToolStripMenuItem";
-            this.returnDashboardsToolStripMenuItem.Size = new System.Drawing.Size(180, 29);
+            this.returnDashboardsToolStripMenuItem.Size = new System.Drawing.Size(230, 54);
             this.returnDashboardsToolStripMenuItem.Text = "Return Dashboards";
             // 
             // completeDashboardDetailsToolStripMenuItem
             // 
+            this.completeDashboardDetailsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("completeDashboardDetailsToolStripMenuItem.Image")));
+            this.completeDashboardDetailsToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.completeDashboardDetailsToolStripMenuItem.Name = "completeDashboardDetailsToolStripMenuItem";
-            this.completeDashboardDetailsToolStripMenuItem.Size = new System.Drawing.Size(256, 29);
+            this.completeDashboardDetailsToolStripMenuItem.Size = new System.Drawing.Size(306, 54);
             this.completeDashboardDetailsToolStripMenuItem.Text = "Complete Dashboard Details";
             // 
             // exitToolStripMenuItem
             // 
+            this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
+            this.exitToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(55, 29);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(105, 54);
             this.exitToolStripMenuItem.Text = "Exit";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1185, 450);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
